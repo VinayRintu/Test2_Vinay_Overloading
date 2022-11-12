af97bba3173b9a1cab9e7c496c0f6287fa85cb46
Rintu <Rintu@VINDUJA>
2022-11-12 15:03:19 +0530

    Add project files.

---
+++ Test2_Vinay_Overloading/MethodOverloadingCalculations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2_Vinay_Overloading
+{
+    public class MethodOverloadingCalculations
+    {
+        public int Add(int number1,int number2)
+        {
+            int total=number1 + number2;
+            return total;
+        }
+        public void Add(int number1, int number2,int number3)
+        {
+            int total = number1 + number2 + number3;
+            Console.WriteLine("Total of Three Integers : "+total);
+        }
+        public string Add(string firstName, string lastName)
+        {
+            string fullName=firstName + lastName;
+            return fullName;
+        }        
+    }
+}
---
+++ Test2_Vinay_Overloading/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test2_Vinay_Overloading;
+
+Console.WriteLine("Hello, World!");
+
+MethodOverloadingCalculations objectOverloading = new MethodOverloadingCalculations();
+double sumOfTwoIntegers= objectOverloading.Add(10, 10);
+Console.WriteLine("Sum Of Two Integers  : "+ sumOfTwoIntegers);
+
+string fullName= objectOverloading.Add("Ravula", "Vinay");
+Console.WriteLine("Adding Firstname And Lastname : "+fullName);
+
+ objectOverloading.Add(14, 15, 13);
